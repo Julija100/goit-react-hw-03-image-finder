27e6abe3ade9c98de120087d2b1f7f05e4cb2b4c
Julija Faltina <dashinicha@gmail.com>
2021-08-31 17:03:06 +0300

    added Icon Button

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -2,7 +2,8 @@ import { Component } from 'react';
 import PropTypes from 'prop-types';
 import ImageGalleryItem from '../ImageGalleryItem/ImageGalleryItem';
 import Modal from '../Modal/Modal';
-import StyledImageModal from '../Modal'
+import StyledImageModal from '../Modal/StyledImageModal'
+import StyledImageGallery from './StyledImageGallery';
 
 
 class ImageGallery extends Component {
